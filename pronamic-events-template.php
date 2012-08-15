7a96d09e1dd0758afa00e34e8b941166a4e83507
Remco Tolsma <info@remcotolsma.nl>
2012-08-15 17:01:43 +0200

    Extended all the template functions with $post_id parameter.

--- pronamic-events-template.php
+++ pronamic-events-template.php
@@ -3,14 +3,11 @@
 /**
  * Return formatted start date
  */
-function pronamic_get_the_start_date( $format = null ) {
-	global $post;
+function pronamic_get_the_start_date( $format = null, $post_id = null ) {
+	$format = ( null === $format ) ? get_option( 'date_format' ) : $format;
+	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
 
-	if( $format == null ) {
-		$format = get_option( 'date_format' );
-	}
-
-	$start_date = get_post_meta( $post->ID, '_pronamic_start_date', true );
+	$start_date = get_post_meta( $post_id, '_pronamic_start_date', true );
 
 	return date_i18n( $format, $start_date );
 }
@@ -18,17 +15,17 @@ function pronamic_get_the_start_date( $format = null ) {
 /**
  * Echo formatted start date
  */
-function pronamic_the_start_date( $format = null ) {
-	echo pronamic_get_the_start_date( $format );
+function pronamic_the_start_date( $format = null, $post_id = null ) {
+	echo pronamic_get_the_start_date( $format, $post_id );
 }
 
 /**
  * Conditional tag for start date
  */
-function pronamic_has_start_date() {
-	global $post;
+function pronamic_has_start_date( $post_id = null ) {
+	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
 
-	$start_date = get_post_meta( $post->ID, '_pronamic_start_date', true );
+	$start_date = get_post_meta( $post_id, '_pronamic_start_date', true );
 
 	return ! empty( $start_date );
 }
@@ -38,14 +35,11 @@ function pronamic_has_start_date() {
 /**
  * Return formatted end date
  */
-function pronamic_get_the_end_date( $format = null ) {
-	global $post;
+function pronamic_get_the_end_date( $format = null, $post_id = null ) {
+	$format = ( null === $format ) ? get_option( 'date_format' ) : $format;
+	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
 
-	if( $format == null ) {
-		$format = get_option( 'date_format' );
-	}
-
-	$end_date = get_post_meta( $post->ID, '_pronamic_end_date', true );
+	$end_date = get_post_meta( $post_id, '_pronamic_end_date', true );
 
 	return date_i18n( $format, $end_date );
 }
@@ -53,15 +47,15 @@ function pronamic_get_the_end_date( $format = null ) {
 /**
  * Echo formatted end date
  */
-function pronamic_the_end_date( $format = null ) {
-	echo pronamic_get_the_end_date( $format );
+function pronamic_the_end_date( $format = null, $post_id = null ) {
+	echo pronamic_get_the_end_date( $format, $post_id );
 }
 
 /**
  * Conditional tag for end date
  */
-function pronamic_has_end_date() {
-	global $post;
+function pronamic_has_end_date( $post_id = null ) {
+	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
 
 	$end_date = get_post_meta( $post->ID, '_pronamic_end_date', true );
 
@@ -75,17 +69,17 @@ function pronamic_has_end_date() {
  * 
  * @return string
  */
-function pronamic_get_the_location() {
-	global $post;
+function pronamic_get_the_location( $post_id = null ) {
+	$post_id = ( null === $post_id ) ? get_the_ID() : $post_id;
 
-	return get_post_meta( $post->ID, '_pronamic_location', true );
+	return get_post_meta( $post_id, '_pronamic_location', true );
 }
 
 /**
  * Echo the location of the post
  */
-function pronamic_the_location() {
-	echo pronamic_get_the_location();
+function pronamic_the_location( $post_id = null ) {
+	echo pronamic_get_the_location( $post_id );
 }
 
 /**
@@ -93,8 +87,8 @@ function pronamic_the_location() {
  * 
  * @return boolean true if post has location, false otherwise
  */
-function pronamic_has_location() {
-	$location = pronamic_get_the_location();
+function pronamic_has_location( $post_id = null ) {
+	$location = pronamic_get_the_location( $post_id );
 
 	return ! empty( $location );
 }
