e13adc3068892e3f372c22cb42bacfb0fa793ea6
Remco Tolsma <info@remcotolsma.nl>
2012-08-16 16:37:04 +0200

    Improved the check on an events query.

--- pronamic-events.php
+++ pronamic-events.php
@@ -270,18 +270,28 @@ add_action( 'save_post', 'pronamic_events_save_post' );
 ////////////////////////////////////////////////////////////
 
 /**
+ * Check if the query is requesting event post type
+ * 
+ * @param WP_Query $query
+ * @return boolean true if events are queried, false otherwises
+ */
+function is_pronamic_events_query( WP_Query $query ) {
+	$is_pronamic_events = false;
+
+	if( $query->is_archive() ) {
+		$object = $query->get_queried_object();
+
+		$is_pronamic_events = isset( $object, $object->name ) && $object->name == 'pronamic_event';
+	}
+
+	return $is_pronamic_events;
+}
+
+/**
  * Customize query for the archive page
  */
 function pronamic_events_query( $query ) {
-	$is_pronamic_events = (
-		$query->is_post_type_archive( 'pronamic_event' )
-			||
-		$query->is_tax( 'pronamic_event_category' )
-			||
-		$query->get( 'post_type') == 'pronamic_event'
-	);
-
-	if( ! is_admin() && $is_pronamic_events ) {
+	if( ! is_admin() && is_pronamic_events_query( $query ) ) {
 		$meta_query_extra = array(
 			array(
 				'key' => '_pronamic_end_date' ,
