509f8f60d7862e407a2716d9d258b1dc8a57462f
Remco Tolsma <info@remcotolsma.nl>
2013-03-06 11:16:49 +0100

    WordPress Coding Standards optimizations.

--- pronamic-events.php
+++ pronamic-events.php
@@ -528,35 +528,35 @@ function pronamic_events_input_text( $args ) {
 function pronamic_events_gform_post_data( $post_data, $form, $lead ) {
 	$start_date = $end_date = null;
 
-	if( isset( $post_data['post_custom_fields'] ) ) {
+	if ( isset( $post_data['post_custom_fields'] ) ) {
 		$fields =& $post_data['post_custom_fields'];
 
 		// Start date
-		if( isset( $fields['_pronamic_start_date_date'] ) ) {
+		if ( isset( $fields['_pronamic_start_date_date'] ) ) {
 			$start_date = $fields['_pronamic_start_date_date'];
 
-			if( isset( $fields['_pronamic_start_date_time'] ) ) {
+			if ( isset( $fields['_pronamic_start_date_time'] ) ) {
 				$start_date .= ' ' . $fields['_pronamic_start_date_time'];
 			}
 			
 			$start_timestamp = strtotime( $start_date );
 
-			if( $start_timestamp !== false ) {
+			if ( $start_timestamp !== false ) {
 				$fields['_pronamic_start_date'] = $start_timestamp;
 			}
 		}
 
 		// End date
-		if( isset( $fields['_pronamic_end_date_date'] ) ) {
+		if ( isset( $fields['_pronamic_end_date_date'] ) ) {
 			$end_date = $fields['_pronamic_end_date_date'];
 	
-			if( isset( $fields['_pronamic_end_date_time'] ) ) {
+			if ( isset( $fields['_pronamic_end_date_time'] ) ) {
 				$end_date .= ' ' . $fields['_pronamic_end_date_time'];
 			}
 			
 			$end_timestamp = strtotime( $end_date );
 
-			if( $end_timestamp !== false ) {
+			if ( $end_timestamp !== false ) {
 				$fields['_pronamic_end_date'] = $end_timestamp;
 			}
 		}
