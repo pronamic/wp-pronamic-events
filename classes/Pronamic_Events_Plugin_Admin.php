78f06172fbafdc87a20d275c194499c00ec90369
Remco Tolsma <info@remcotolsma.nl>
2013-06-25 15:51:05 +0200

    Some small improvements regarding the enqueueing of jQuery UI i18n files.

--- classes/Pronamic_Events_Plugin_Admin.php
+++ classes/Pronamic_Events_Plugin_Admin.php
@@ -146,26 +146,49 @@ class Pronamic_Events_Plugin_Admin {
 		
 			wp_enqueue_style( 'jquery-ui-datepicker', plugins_url( '/jquery-ui/themes/base/jquery.ui.all.css', $this->plugin->file ) );
 
-			// jQuery UI i18n
-			// https://github.com/jquery/jquery-ui/tree/master/ui/i18n
-
-			$locale = get_locale(); // nl_NL
-			$locale = str_replace( '_', '-', $locale ); // nl-NL
-
-			$names  = array( $locale, 
-				$locale, // nl-NL
-				substr( $locale, 0, 2 ) // nl
-			);
-
-			foreach ( $names as $name ) {
-				$path = sprintf( '/jquery-ui/languages/jquery.ui.datepicker-%s.js', $name );
-				$file = $this->plugin->dirname . $path;
-
-				if ( is_readable( $file ) ) {
-					wp_enqueue_script( 'jquery-ui-datepicker-' . $name, plugins_url( $path, $this->plugin->file ) );
-				}
-			}				
+			self::enqueue_jquery_ui_i18n_path( 'datepicker' );
 		}
+	}
+
+	//////////////////////////////////////////////////
+
+	/**
+	 * Get jQuery UI i18n file
+	 * https://github.com/jquery/jquery-ui/tree/master/ui/i18n
+	 * 
+	 * @param string $module
+	 */
+	private function enqueue_jquery_ui_i18n_path( $module ) {
+		$result = false;
+
+		// Retrive the WordPress locale, for example 'en_GB'
+		$locale = get_locale();
+		
+		// jQuery UI uses 'en-GB' notation, replace underscore with hyphen 
+		$locale = str_replace( '_', '-', $locale );
+		
+		// Create an search array with two variants 'en-GB' and 'en'
+		$search = array(
+			$locale, // en-GB
+			substr( $locale, 0, 2 ) // en
+		);
+		
+		foreach ( $search as $name ) {
+			$path = sprintf( '/jquery-ui/languages/jquery.ui.%s-%s.js', $module, $name );
+
+			$file = $this->plugin->dirname . $path;
+
+			if ( is_readable( $file ) ) {
+				wp_enqueue_script( 
+					'jquery-ui-' . $module . '-' . $name,
+					plugins_url( $path, $this->plugin->file ) 
+				);
+
+				break;
+			}
+		}
+		
+		return $result;
 	}
 
 	//////////////////////////////////////////////////
