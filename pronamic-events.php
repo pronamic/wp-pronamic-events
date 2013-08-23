f280f64f266b49334bd91dc127c7b4ada9bda94b
Remco Tolsma <info@remcotolsma.nl>
2013-08-23 09:28:19 +0200

    Added Brazilian Portuguese translation thanks to Gustavo Magalhães.

--- pronamic-events.php
+++ pronamic-events.php
@@ -4,7 +4,7 @@ Plugin Name: Pronamic Events
 Plugin URI: http://pronamic.eu/wordpress/events/
 Description: This plugin add some basic Event functionality to WordPress
 
-Version: 0.2.1
+Version: 0.2.2
 Requires at least: 3.0
 
 Author: Pronamic
