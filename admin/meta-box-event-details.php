02899de1cdb50dc63dd6a8e9e1614442366b1a40
Remco Tolsma <info@remcotolsma.nl>
2014-05-16 14:37:22 +0200

    Use the default WordPress form table in the event details meta box.

--- admin/meta-box-event-details.php
+++ admin/meta-box-event-details.php
@@ -24,40 +24,51 @@ if ( pronamic_has_end_date( ) ) {
 
 ?>
 
-<div class="pronamic-section pronamic-section-first">
-	<div>
-		<label for="pronamic_start_date"><?php _e( 'Start Date:', 'pronamic_events' ); ?></label> <br />
-		<input class="pronamic_date" type="text" id="pronamic_start_date" name="pronamic_start_date" value="<?php echo $start_date; ?>" size="14" />
-		<input type="text" id="pronamic_start_time" name="pronamic_start_time" value="<?php echo $start_time; ?>" size="6" placeholder="00:00" />
-	</div>
+<table class="form-table">
+	<tbody>
+		<tr>
+			<th scope="row">
+				<label for="pronamic_start_date"><?php _e( 'Start Date', 'pronamic_events' ); ?></label>
+			</th>
+			<td>
+				<input class="pronamic_date" type="text" id="pronamic_start_date" name="pronamic_start_date" value="<?php echo $start_date; ?>" size="14" />
+				<input type="text" id="pronamic_start_time" name="pronamic_start_time" value="<?php echo $start_time; ?>" size="6" placeholder="00:00" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">
+				<label for="pronamic_end_date"><?php _e( 'End Date', 'pronamic_events' ); ?></label>
+			</th>
+			<td>
+				<input class="pronamic_date" type="text" id="pronamic_end_date" name="pronamic_end_date" value="<?php echo $end_date; ?>" size="14"  />
+				<input type="text" id="pronamic_end_time" name="pronamic_end_time" value="<?php echo $end_time; ?>" size="6" placeholder="00:00" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">
+				<label for="pronamic_location"><?php _e( 'Location', 'pronamic_events' ); ?></label>
+			</th>
+			<td>
+				<input type="text" id="pronamic_location" name="pronamic_location" value="<?php echo get_post_meta( $post->ID, '_pronamic_location', true ); ?>" size="25" />
+			</td>
+		</tr>
+		<tr>
+			<th scope="row">
+				<label for="pronamic_event_url"><?php _e( 'Website', 'pronamic_events' ); ?></label> <br />
+			</th>
+			<td>
+				<input type="url" id="pronamic_event_url" name="pronamic_event_url" value="<?php echo get_post_meta( $post->ID, '_pronamic_event_url', true ); ?>" size="25" />
+			</td>
+		</tr>
+	</tbody>
+</table>
 
-	<div>
-		<label for="pronamic_end_date"><?php _e( 'End Date:', 'pronamic_events' ); ?></label> <br />
-		<input class="pronamic_date" type="text" id="pronamic_end_date" name="pronamic_end_date" value="<?php echo $end_date; ?>" size="14"  />
-		<input type="text" id="pronamic_end_time" name="pronamic_end_time" value="<?php echo $end_time; ?>" size="6" placeholder="00:00" />
-	</div>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		var field = $('.pronamic_date');
 
-	<script type="text/javascript">
-		jQuery(document).ready(function($) {
-			var field = $('.pronamic_date');
-
-			field.datepicker({
-				dateFormat: 'dd-mm-yy'
-			});
+		field.datepicker({
+			dateFormat: 'dd-mm-yy'
 		});
-	</script>
-</div>
-
-<div class="pronamic-section">
-	<div>
-		<label for="pronamic_location"><?php _e( 'Location:', 'pronamic_events' ); ?></label> <br />
-		<input type="text" id="pronamic_location" name="pronamic_location" value="<?php echo get_post_meta( $post->ID, '_pronamic_location', true ); ?>" size="25" />
-	</div>
-</div>
-
-<div class="pronamic-section pronamic-section-last">
-	<div>
-		<label for="pronamic_event_url"><?php _e( 'Website:', 'pronamic_events' ); ?></label> <br />
-		<input type="url" id="pronamic_event_url" name="pronamic_event_url" value="<?php echo get_post_meta( $post->ID, '_pronamic_event_url', true ); ?>" size="25" />
-	</div>
-</div>
+	});
+</script>
