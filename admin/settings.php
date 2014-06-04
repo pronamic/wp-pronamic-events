7a9870fd1f822dae002886577a8f216f1427b6b0
Remco Tolsma <info@remcotolsma.nl>
2014-06-04 14:02:27 +0200

    Removed screen icon, no longer supported.

--- admin/settings.php
+++ admin/settings.php
@@ -6,8 +6,6 @@ flush_rewrite_rules();
 
 ?>
 <div class="wrap">
-	<?php screen_icon(); ?>
-
 	<h2><?php echo get_admin_page_title(); ?></h2>
 
 	<form name="form" action="options.php" method="post">
