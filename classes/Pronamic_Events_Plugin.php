5c78770c57b4a7df8fa671085affc84961887345
Remco Tolsma <info@remcotolsma.nl>
2014-10-31 15:45:54 +0100

    Only update published posts.

--- classes/Pronamic_Events_Plugin.php
+++ classes/Pronamic_Events_Plugin.php
@@ -382,7 +382,7 @@ class Pronamic_Events_Plugin {
 			$wpdb->update(
 				$wpdb->posts,
 				array( 'post_status' => 'passed' ),
-				array( 'ID' => $post_id ),
+				array( 'ID' => $post_id, 'post_status' => 'publish' ),
 				array( '%s' ),
 				array( '%d' )
 			);
