681e9ed738aaa8b3cdbf1222e4bff681c701c9f1
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-03-01 11:17:17 +0100

    Simplify multi blocks plugin file/directory structure.

---
+++ blocks/event-end-date/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9df634a8c9ed7809d228');
---
+++ blocks/event-location/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '210e17967d42ae9b6ae0');
