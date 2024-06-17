251bff341412f540e774840fc048684feebba10a
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-06-17 14:06:12 +0200

    Update pronamic-events.php

--- pronamic-events.php
+++ pronamic-events.php
@@ -1,22 +1,27 @@
 <?php
-/*
-Plugin Name: Pronamic Events
-Plugin URI: https://www.pronamic.eu/plugins/pronamic-events/
-Description: This plugin add some basic Event functionality to WordPress.
-
-Version: 1.3.0
-Requires at least: 3.0
-
-Author: Pronamic
-Author URI: https://www.pronamic.eu/
-
-Text Domain: pronamic-events
-Domain Path: /languages/
-
-License: GPL
-
-GitHub URI: https://github.com/pronamic/wp-pronamic-events
-*/
+/**
+ * Plugin Name: Pronamic Events
+ * Plugin URI: https://www.pronamic.eu/plugins/pronamic-events/
+ * Description: This plugin add some basic Event functionality to WordPress.
+ *
+ * Version: 1.3.0
+ * Requires at least: 3.0
+ *
+ * Author: Pronamic
+ * Author URI: https://www.pronamic.eu/
+ *
+ * Text Domain: pronamic-events
+ * Domain Path: /languages/
+ *
+ * License: GPL-2.0-or-later
+ *
+ * GitHub URI: https://github.com/pronamic/wp-pronamic-events
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2024 Pronamic
+ * @license   GPL-2.0-or-later
+ * @package   Pronamic\WpPronamicEvents
+ */
 
 /**
  * Autoload
