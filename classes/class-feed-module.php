33e6be1d2ff0558f51536befa69bde66b0eed84b
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2024-07-01 14:35:04 +0200

    More type safe approuch.

--- classes/class-feed-module.php
+++ classes/class-feed-module.php
@@ -12,46 +12,37 @@ class Pronamic_Events_FeedModule {
 	private $plugin;
 
 	/**
-	 * Constructs and initializes an Pronamic Events feed module object.
+	 * Construct feed module.
+	 * 
+	 * @param Pronamic_Events_Plugin $plugin Plugin.
 	 */
 	public function __construct( Pronamic_Events_Plugin $plugin ) {
 		$this->plugin = $plugin;
 
-		// Actions.
 		add_action( 'rss2_ns', array( $this, 'rss2_ns' ) );
 		add_action( 'rss2_item', array( $this, 'rss2_item' ) );
 	}
 
-    /**
-     * Determine if the post type supports the Pronamic Events feed module.
-     *
-     * @return boolean
-     */
-    protected function is_supported_post_type($post_type = null) {
-        if ( ! $post_type ) {
-            return false;
-        }
-
-        $post_types = (array) $post_type;
-
-        foreach ( $post_types as $post_type ) {
-            if ( post_type_supports( $post_type, 'pronamic_event' ) ) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
 	/**
 	 * RSS2 namespaces.
 	 *
-	 * @see https://theeventscalendar.com/knowledgebase/customize-rss-feed/
-	 * @see http://web.resource.org/rss/1.0/modules/event/
-	 * @see https://github.com/WordPress/WordPress/blob/4.9/wp-includes/feed-rss2.php#L30-L37
+	 * @link https://theeventscalendar.com/knowledgebase/customize-rss-feed/
+	 * @link http://web.resource.org/rss/1.0/modules/event/
+	 * @link https://github.com/WordPress/WordPress/blob/4.9/wp-includes/feed-rss2.php#L30-L37
+	 * @return void
 	 */
 	public function rss2_ns() {
-		if ( ! $this->is_supported_post_type( get_query_var( 'post_type' ) ) ) {
+		$post_types = \wp_parse_list( \get_query_var( 'post_type' ) );
+
+		if ( ! \is_array( $post_types ) ) {
+			return;
+		}
+
+		$event_post_types = \array_filter( $post_types, function( $post_type ) {
+			return \post_type_supports( $post_type, 'pronamic_event' );
+		} );
+
+		if ( 0 === \count( $event_post_types ) ) {
 			return;
 		}
 
@@ -61,12 +52,19 @@ class Pronamic_Events_FeedModule {
 	/**
 	 * RSS2 item.
 	 *
-	 * @see https://theeventscalendar.com/knowledgebase/customize-rss-feed/
-	 * @see http://web.resource.org/rss/1.0/modules/event/
-	 * @see https://github.com/WordPress/WordPress/blob/4.9/wp-includes/feed-rss2.php#L113-L120
+	 * @link https://theeventscalendar.com/knowledgebase/customize-rss-feed/
+	 * @link http://web.resource.org/rss/1.0/modules/event/
+	 * @link https://github.com/WordPress/WordPress/blob/4.9/wp-includes/feed-rss2.php#L113-L120
+	 * @return void
 	 */
 	public function rss2_item() {
-		if ( ! $this->is_supported_post_type( get_post_type() ) ) {
+		$post_type = \get_post_type();
+
+		if ( false === $post_type ) {
+			return;
+		}
+
+		if ( ! \post_type_supports( $post_type, 'pronamic_event' ) ) {
 			return;
 		}
 
